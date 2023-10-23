cf1046b67d7d702421b9cf5642e7dd57b0bd3ef1
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-10-23 14:44:43 +0700

    auth

---
+++ T2207A_API/Models/User/UserRegister.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace T2207A_API.Models.User
+{
+	public class UserRegister
+	{
+		[Required]
+		public string email { get; set; }
+		[Required]
+		public string full_name { get; set; }
+		[Required]
+		[MinLength(6)]
+		public string password { get; set; }
+	}
+}
+
--- T2207A_API/Program.cs
+++ T2207A_API/Program.cs
@@ -16,7 +16,12 @@ builder.Services.AddCors(options =>
 });
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson( options=>
+            options.SerializerSettings.ReferenceLoopHandling
+            = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+    );
+    
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
